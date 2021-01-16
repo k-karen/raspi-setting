e7445fd196998e4a273790db8d08dfbb68cf9a32
kujo-karen <birth.colors@gmail.com>
2021-01-16 18:35:22 +0900

    safe exec

--- wpa_passphrase.rb
+++ wpa_passphrase.rb
@@ -3,12 +3,14 @@ ssid = file.find { |str| str.match(/\A\s+ssid="(.+)"\z/)} && $1
 psk = file.find { |str| str.match(/\A\s+psk="(.+)"\z/)} && $1
 hash = `sudo sh -c 'wpa_passphrase "#{ssid}" "#{psk}"'`.chomp.split("\n").find { |str| str.match(/\A\s+psk=(.+)\z/)} && $1
 
-File.open('/etc/wpa_supplicant/wpa_supplicant.conf', 'w') do |f|
-  file.each do |fi|
-    if fi.match?('psk=')
-      f.puts(fi.sub(/(".+")/, hash))
-    else
-      f.puts(fi)
+if file.any? { |line| line.match?(/psk="(.+)"/) }
+  File.open('/etc/wpa_supplicant/wpa_supplicant.conf', 'w') do |f|
+    file.each do |fi|
+      if fi.match?('psk=')
+        f.puts(fi.sub(/(".+")/, hash))
+      else
+        f.puts(fi)
+      end
     end
   end
 end
