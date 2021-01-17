42ea4b5a1bb6094055eda5a7c76c1bc7b64fbcd7
kujo-karen <birth.colors@gmail.com>
2021-01-18 00:09:31 +0900

    create setup

--- wpa_pass_hasher.rb
+++ wpa_pass_hasher.rb
@@ -3,7 +3,7 @@ ssid = file.find { |str| str.match(/\A\s+ssid="(.+)"\z/)} && $1
 psk = file.find { |str| str.match(/\A\s+psk="(.+)"\z/)} && $1
 hash = `sudo sh -c 'wpa_passphrase "#{ssid}" "#{psk}"'`.chomp.split("\n").find { |str| str.match(/\A\s+psk=(.+)\z/)} && $1
 
-if file.any? { |line| line.match?(/psk="(.+)"/) }
+if hash && file.any? { |line| line.match?(/psk="(.+)"/) }
   File.open('/etc/wpa_supplicant/wpa_supplicant.conf', 'w') do |f|
     file.each do |fi|
       if fi.match?('psk=')
